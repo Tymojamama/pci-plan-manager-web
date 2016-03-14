624533540b12932c8aaba23ae7e216542d6a3c23
Zach417 <zallen@pension-consultants.com>
2016-03-14 17:48:04 -0500

    Changes for production

--- server/services/PlanManager/settings.js
+++ server/services/PlanManager/settings.js
@@ -1,4 +1,2 @@
-//module.exports = { host: "ec2-52-37-104-20.us-west-2.compute.amazonaws.com", port: 80 } // Production
-module.exports = { host: "localhost", port: 8080 } // Development
-
-/*** DONT COMMIT UNTIL YOU FIGURE OUT WHERE TO STORE SMTP CONFIG (SUCH AS EMAIL/PASSWORD) ***/
+modle.exports = { host: "ec2-52-37-104-20.us-west-2.compute.amazonaws.com", port: 80 } // Production
+//module.exports = { host: "localhost", port: 8080 } // Development
